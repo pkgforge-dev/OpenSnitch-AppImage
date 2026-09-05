69a09e4b22562099915bf2b2271076efe0b3406b
Samueru-sama <xdglawyer@outlook.com>
2026-09-05 19:41:15 -0400

    package app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,16 +6,95 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	go                  \
+	libcap              \
+	libmnl              \
+	libnetfilter_queue  \
+	libnfnetlink        \
+	protobuf            \
+	python              \
+	python-build        \
+	python-grpcio       \
+	python-installer    \
+	python-jaraco.text  \
+	python-notify2      \
+	python-packaging    \
+	python-pyinotify    \
+	python-pyqt6        \
+	python-protobuf     \
+	python-qt-material  \
+	python-setuptools   \
+	python-slugify      \
+	python-wheel        \
+	qt6-tools
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+# OpenSnitch pins old grpc/protobuf versions in go.mod, newer protoc
+# plugins generate code that does not build against them
+echo "Installing pinned protoc plugins..."
+echo "---------------------------------------------------------------"
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0
+PATH=$(go env GOPATH)/bin:$PATH
+export PATH
 
 # If the application needs to be manually built that has to be done down here
+echo "Building opensnitch..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/evilsocket/opensnitch.git ./opensnitch && (
+	cd ./opensnitch
+	# Build the latest stable tag
+	TAG=$(git tag --list 'v*' --sort=-v:refname | grep -vi 'rc\|alpha\|beta' | head -n 1)
+	git checkout "$TAG"
+	VERSION="${TAG#v}"
+	echo "$VERSION" > ~/version
+)
+
+# generate the Go protobuf files, the python ones are already committed
+(
+	cd ./opensnitch/proto
+	protoc -I. ui.proto \
+		--go_out=../daemon/ui/protocol/ --go-grpc_out=../daemon/ui/protocol/ \
+		--go_opt=paths=source_relative --go-grpc_opt=paths=source_relative
+)
+
+# build the daemon
+(
+	cd ./opensnitch/daemon
+	go build -o opensnitchd .
+	cp opensnitchd /usr/bin/
+)
+
+# the gui lists the i18n dir at runtime, so the .qm files are mandatory
+(
+	cd ./opensnitch/ui/i18n
+	./generate_i18n.sh
+	for lang in ./locales/*/; do
+		lang=${lang#./locales/}
+		lang=${lang%/}
+		mkdir -p ../opensnitch/i18n/"$lang"
+		cp ./locales/"$lang"/opensnitch-"$lang".qm ../opensnitch/i18n/"$lang"
+	done
+)
+
+# build and install the gui
+(
+	cd ./opensnitch/ui
+	python -m build --wheel --no-isolation
+	python -m installer --prefix=/usr ./dist/*.whl
+)
+
+# install the config files, these are copied to a writable location at runtime
+mkdir -p /usr/share/opensnitchd/rules /usr/share/opensnitchd/tasks
+cp -v ./opensnitch/daemon/data/default-config.json /usr/share/opensnitchd/
+cp -v ./opensnitch/daemon/data/system-fw.json /usr/share/opensnitchd/
+cp -v ./opensnitch/daemon/data/network_aliases.json /usr/share/opensnitchd/
+cp -v ./opensnitch/daemon/data/rules/*.json /usr/share/opensnitchd/rules/
+cp -v ./opensnitch/daemon/data/tasks/tasks.json /usr/share/opensnitchd/tasks/
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,35 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export APPNAME=OpenSnitch
+export ICON=/usr/share/icons/hicolor/scalable/apps/opensnitch-ui.svg
+export DESKTOP=/usr/share/applications/opensnitch_ui.desktop
+export DEPLOY_PYTHON=1
+export ALWAYS_SOFTWARE=1
+export STRACE_BINARY=opensnitch-ui
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/opensnitch* \
+	/usr/bin/setcap      \
+	/usr/bin/getcap      \
+	/usr/share/opensnitchd
 
-# Additional changes can be done in between here
+# add markers in the config file that get replaced at runtime
+# ebpf is kernel specific and not portable, use proc instead
+sed -i \
+	-e 's|/etc/opensnitchd|@CONFIGDIR@|g'                            \
+	-e 's|/var/log/opensnitchd.log|@CONFIGDIR@/opensnitchd.log|'     \
+	-e 's|"ProcMonitorMethod": *"ebpf"|"ProcMonitorMethod": "proc"|' \
+	./AppDir/share/opensnitchd/default-config.json
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
